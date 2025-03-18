eb41a2be5f9d18bca9d733c2fb330b2c2dbfe6c3
LiizethRR <rmm.che@gmail.com>
2025-03-18 15:46:08 -0600

    Descripción de los cambios realizados

--- app/routes/participacionRoutes.js
+++ app/routes/participacionRoutes.js
@@ -1,9 +1,11 @@
 // routes/cursosRoutes.js
 const express = require('express');
 const router = express.Router();
-const { registrarParticipaciones, obtenerEmpleadosConCursos} = require('../controllers/participacionController');
+const { registrarParticipaciones, obtenerEmpleadosConCursos, obtenerActividadesEmpleado} = require('../controllers/participacionController');
 
 
 router.post('/registrar/participaciones', registrarParticipaciones);
 router.get('/participacion/empleados-con-cursos', obtenerEmpleadosConCursos);
+
+router.get('/participacion/empleado/:clave_empleado', obtenerActividadesEmpleado);
 module.exports = router;
